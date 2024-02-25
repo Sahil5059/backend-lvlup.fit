764ceced11dfb3f14c81654ebc65255c5eeb8a82
dedic <dedicatedgamer59@gmail.com>
2024-02-25 12:43:07 +0530

    setting up server

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,10 @@
 //imports
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 //1.exporting-express-app
-export const app = express(); //now move to "./server.ts"
+export const app = express();
+//now move to "./server.ts"
 
 //3.
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 //imports
 import {app} from './app';
-require("dotenv").config(); //create a ".env" file in root-directory and write "PORT = 8000" and then come back here
+require("dotenv").config();
 
 //2.creating-server
+//create a ".env" file in root-directory and write "PORT = 8000" and then come back here
 app.listen(process.env.PORT, () => {
     console.log(`Server is connected with port ${process.env.PORT}`);
-}); //now add the following line in the "scripts" section of "package.json" before hitting "npm run dev": ""dev": "ts-node-dev --respawn --transpile-only server.ts"". Now move to app.ts
+});
+//now add the following line in the "scripts" section of "package.json" before hitting "npm run dev": ""dev": "ts-node-dev --respawn --transpile-only server.ts"". Now move to app.ts
